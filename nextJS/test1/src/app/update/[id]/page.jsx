2413a8ebb1f0d0059982b566abd5570d1d0de5de
Lim J <149343911+LimJ2023@users.noreply.github.com>
2024-09-10 21:35:55 +0900

    넥스트js입문

---
+++ nextJS/test1/src/app/update/[id]/page.jsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Update() {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const router = useRouter();
+    const params = useParams();
+    useEffect(() => {
+        fetch("http://localhost:9999/topics/" + params.id)
+            .then((res) => res.json())
+            .then((result) => {
+                console.log(result);
+                setTitle(result.title);
+                setBody(result.body);
+            });
+    }, []);
+    return (
+        <>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    const title = e.target.title.value;
+                    const body = e.target.body.value;
+                    const options = {
+                        method: "PATCH",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({ title, body }),
+                    };
+                    fetch("http://localhost:9999/topics/" + params.id, options)
+                        .then((res) => res.json())
+                        .then((result) => {
+                            console.log("result : ", result);
+                            const lastId = result.id;
+                            router.push(`/read/${lastId}`);
+                            router.refresh();
+                        });
+                }}
+            >
+                <p>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </p>
+                <p>
+                    <textarea
+                        name="body"
+                        placeholder="body"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                    ></textarea>
+                </p>
+                <p>
+                    <input type="submit" value={"Update"} />
+                </p>
+            </form>
+        </>
+    );
+}
